86e6b6ea1edc8257535fd4c84fc7adfd3c0d0d7a
Oluwafemi Tairu <tairuoluwafemi09@gmail.com>
2021-07-16 00:32:52 +0100

    working code

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Object Detection from Video
+
+### How to use the application
+
+\*\*\* Ensure you are connected to the internet
+
+1. Install Python 3.7. You can download from the [official Python website](https://www.python.org/)
+2. Clone this repository
+3. Open a terminal and change directory to this repo's directory on your system
+4. Run `bash pip install virtualenv` or `bash pip3 install virtualenv`
+5. Run `bash virtualenv env`
+6. Run `bash source env/bin/activate` on a Mac/Linux or `bash env/Scripts/activate` on Windows
+7. Run `bash pip install -r requirements.txt` or `bash pip3 install -r requirements.txt`
+8. Download the file [here](https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5) or [here](https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo-tiny.h5) if you prefer light weight model and speed and put inside the `models` folder
+9. Run `bash python main.py`
+
+### Video Capture
+
+```python
+video_path = object_detector.detectObjectsFromVideo(
+    camera_input=video_capture,
+    # output_file_path=os.path.join(BASE_DIR, "camera_detected_video"),
+    frames_per_second=1,
+    log_progress=True,
+    minimum_percentage_probability=40,
+    save_detected_video=False,
+    display_box=True,
+    display_object_name=True,
+    display_percentage_probability=True,
+    thread_safe=True
+)
+```
--- main.py
+++ main.py
@@ -1,16 +1,108 @@
-#
-
-from imageai.Detection import VideoObjectDetection
 import os
+import cv2
+from imageai.Detection import ObjectDetection
 
-execution_path = os.getcwd()
+BASE_DIR = os.getcwd()
 
-detector = VideoObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath(os.path.join(
-    execution_path, "model/resnet50_coco_best_v2.0.1.h5"))
-detector.loadModel()
+object_detector = ObjectDetection()
 
-video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "traffic.mp4"),
-                                             output_file_path=os.path.join(execution_path, "traffic_detected"), frames_per_second=20, log_progress=True)
-print(video_path)
+
+model_path = os.path.join(BASE_DIR, "model/yolo.h5")
+
+object_detector.setModelTypeAsYOLOv3()
+
+if os.path.isfile(model_path) == False:
+    #
+    model_path = os.path.join(BASE_DIR, "model/yolo-tiny.h5")
+
+    object_detector.setModelTypeAsTinyYOLOv3()
+
+object_detector.setModelPath(model_path)
+
+object_detector.loadModel(
+    detection_speed="fast"
+)
+
+video_capture = cv2.VideoCapture(0)
+
+# video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1500)
+# video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 2500)
+
+cv2.namedWindow('Object Detection System',
+                cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
+
+process_this_frame = True
+
+while video_capture.isOpened():
+
+    # Grab a single frame of video
+    frame_exists, frame = video_capture.read()
+
+    if not frame_exists:
+        break
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+
+        detected_image_array, detections = object_detector.detectObjectsFromImage(  # type: ignore
+            input_image=frame,
+            custom_objects=None,
+            input_type="array",
+            output_type="array",
+            minimum_percentage_probability=70,
+            display_percentage_probability=True,
+            display_object_name=True,
+            thread_safe=True,
+        )
+
+        if detections:
+            for eachObject in detections:
+                """
+                {
+                    'name': 'person',
+                    'percentage_probability': '99.9723732471466',
+                    'box_points': [102, 77, 1176, 760] # [x1, y1, x2, y2]
+                }
+                """
+                left, top, right, bottom = eachObject["box_points"]
+
+                # Draw a box around the object
+                cv2.rectangle(
+                    frame,
+                    (left, top),
+                    (right, bottom),
+                    (0, 0, 255),
+                    2
+                )
+
+                # Draw a label with a name below the face
+                cv2.rectangle(
+                    frame,
+                    (left, bottom - 35),
+                    (right, bottom),
+                    (0, 0, 255),
+                    cv2.FILLED
+                )
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(
+                    frame,
+                    eachObject["name"],
+                    (left + 6, bottom - 6),
+                    font,
+                    1.0,
+                    (255, 255, 255),
+                    1
+                )
+
+    process_this_frame = not process_this_frame
+
+    # show video frame
+    cv2.imshow('Object Detection System', frame)
+
+    # ESC key to quit application
+    if cv2.waitKey(30) & 0xff == 27:  # ESC Key
+        print("[INFO] : Terminating application ...")
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
